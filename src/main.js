71c7b5028fc252065e504d9da1d1a1ee5bd20e2a
1049612128 <1049612128@qq.com>
2019-01-02 18:14:54 +0800

    2019/1/2

--- src/main.js
+++ src/main.js
@@ -2,16 +2,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-import routes from './router/router'
+import VueRouter from 'vue-router'
+import routes from './router/'
 import {routerMode} from './config/env'
-import Fastclick fron 'fastclick'
-
+import store from './store'
+import FastClick from 'fastclick'
+if('addEventListener' in document){
+  document.addEventListener('DoMContentLoaded',function(){
+    FastClick.attach(document.body)
+  },false)
+}
 Vue.config.productionTip = false
-
+Vue.use(VueRouter)
+const router =new VueRouter({
+  routes,
+  mode:routerMode,
+  strict:process.env.NODE_ENV!=="product",
+  scrollBehavior(to,from,savedPostion){
+    if(savedPostion){
+      return savedPostion
+    }else{
+      if(from.meta.keepAlive){
+        from.meta.savedPosition=document.body.scrollTop;
+      }
+      return {x:0,y:to.meta.savedPosition||0}
+    }
+  }
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
